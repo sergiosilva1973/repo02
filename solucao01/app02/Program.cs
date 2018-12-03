50ef51b128dd2937981f9d4566974450fc31882a
ssilva <ssilva@ZEUS>
2018-12-03 20:48:27 +0000

    gggg

---
+++ solucao01/app02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace app02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
